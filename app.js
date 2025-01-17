e331797eb6f7b3df64c6b4732bd2cd2b87e3f806
Andrus <andrus.naulainen@ametikool.ee>
2025-01-17 10:20:50 +0200

    add reset button and different game board size options

--- app.js
+++ app.js
@@ -1,62 +1,111 @@
-const cellDivs = Array.from(document.getElementsByClassName('cell'));
+const gameBoardDiv = document.getElementById('game-board');
 const messageDiv = document.getElementById('message');
 const resetBtn = document.getElementById('reset-game');
 
-let nextPlayer, symbols, playerWon, moveCount;
+const gameBoardSize = 5;
+const symbols = ['X', 'O'];
+
+let nextPlayer, playerWon, moveCount, gameState, winningCombinations; 
+
+initWinningCombinations();
 
 initGame();
-
-const winningCombinations = [
-    ['00', '01', '02'], // top row
-    ['10', '11', '12'], // middle row
-    ['20', '21', '22'], // bottom row
-    ['00', '10', '20'], // left column
-    ['01', '11', '21'], // middle column
-    ['02', '12', '22'], // right column
-    ['00', '11', '22'], // top-left to bottom-right diagonal
-    ['02', '11', '20']  // top-right to bottom-left diagonal
-];
-
-let gameState = [[], []];
-
-cellDivs.forEach( cellDiv => {
-    
-    cellDiv.addEventListener('click', e => {
-
-        if ( !e.target.innerText && !playerWon ) {
-
-            moveCount++;
-
-            const move = e.target.dataset.y + e.target.dataset.x;
-            gameState[nextPlayer].push(move);
-
-            e.target.innerText = symbols[nextPlayer];
-
-            if ( hasPlayerWon(gameState[nextPlayer]) ) {
-                playerWon = true;
-                messageDiv.innerText = `${symbols[nextPlayer]} VÕITIS MÄNGU!`;
-            } else if ( moveCount == 9 ) {
-                messageDiv.innerText = `MÄNG JÄI VIIKI!`;
-            }
-
-            nextPlayer = Number(!nextPlayer);
-
-        }
-
-    });
-
-});
 
 resetBtn.addEventListener('click', e => {
     initGame();
 });
 
-function initGame () {
+function initWinningCombinations () {
 
+    winningCombinations = [];
+
+    for ( let i = 0; i < gameBoardSize; i++ ) {
+        
+        let row = [];
+        let col = [];
+        
+        for ( let j = 0; j < gameBoardSize; j++ ) {
+            row.push(`${i}${j}`);
+            col.push(`${j}${i}`);
+        }
+        
+        winningCombinations.push(row);
+        winningCombinations.push(col);
+        
+    }
+    
+    let d1 = [];
+    let d2 = [];
+    
+    for ( let k = 0; k < gameBoardSize; k++ ) {
+        d1.push(`${k}${k}`);
+        d2.push(`${k}${gameBoardSize - 1 - k}`);
+    }
+
+    winningCombinations.push(d1);
+    winningCombinations.push(d2);
+
+    console.log(winningCombinations);
+
+}
+
+function initGame () {
+    
     nextPlayer = 0;
-    symbols = ['X', 'O'];
     playerWon = false;
     moveCount = 0;
+    gameState = [[], []];
+
+    initGameBoard();
+
+}
+
+function initGameBoard () {
+
+    gameBoardDiv.innerHTML = '';
+    messageDiv.innerText = '';
+
+    gameBoardDiv.style.gridTemplateColumns = `repeat(${gameBoardSize}, 64px)`;
+    gameBoardDiv.style.gridTemplateRows = `repeat(${gameBoardSize}, 64px)`;
+
+    for ( let y = 0; y < gameBoardSize; y++ ) {
+
+        for ( let x = 0; x < gameBoardSize; x++ ) {
+
+            const cellDiv = document.createElement('div');
+            cellDiv.classList.add('cell');
+            cellDiv.dataset.y = y;
+            cellDiv.dataset.x = x;
+
+            cellDiv.addEventListener('click', e => {
+                
+                if ( !e.target.innerText && !playerWon ) {
+
+                    moveCount++;
+
+                    const move = e.target.dataset.y + e.target.dataset.x;
+                    gameState[nextPlayer].push(move);
+
+                    e.target.innerText = symbols[nextPlayer];
+
+                    if ( hasPlayerWon(gameState[nextPlayer]) ) {
+                        playerWon = true;
+                        messageDiv.innerText = `${symbols[nextPlayer]} VÕITIS MÄNGU!`;
+                    } else if ( moveCount == gameBoardSize ** 2 ) {
+                        messageDiv.innerText = `MÄNG JÄI VIIKI!`;
+                    }
+        
+                    nextPlayer = Number(!nextPlayer);
+
+                }
+
+            });
+
+            gameBoardDiv.appendChild(cellDiv);
+
+        }
+
+    }
     
 }
 
